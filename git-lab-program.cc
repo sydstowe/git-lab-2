e7f6f7145bff9711f464c6a2c45ac43bbb53578e
Syd Stowe <ss843318@ohio.edu>
2020-01-22 19:53:58 -0500

    Updated program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Syd Stowe>
+ *        Date: <01/22/20>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
